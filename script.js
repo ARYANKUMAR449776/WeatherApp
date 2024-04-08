38111d05a782c0b13999e2724925add8f121d034
ARYANKUMAR449776 <aimpendance@gmail.com>
2024-04-07 18:32:21 -0600

    Updating weather styles

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const city = "Calgary";
  
 // API URL
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
- 
-fetch(apiUrl)
+     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         console.log("Test connection");
         const temperature = data.main.temp;
         const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+
+        // Constructing the icon URL
+        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
  
         // Display weather information
         const DomElements = document.getElementById('DomElements');
         DomElements.innerHTML = `
-            
-            <p> ${temperature}°C </p>
-            <p> ${description} </p>
+            <div class="weather-container">
+                <img src="${iconUrl}" class="weather-icon black-and-white-theme" alt="Weather Icon">
+                <div class="weather-info">
+                    <p>Temperature: ${temperature}°C</p>
+                    <p>Description: ${description}</p>
+                </div>
+            </div>
         `;
     })
     .catch(error => {
